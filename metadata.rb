05bfb65bc0f272ea1723c260f499ea65db9df045
Tim Smith <tsmith84@gmail.com>
2015-09-21 16:42:45 -0700

    Better description and supported platforms

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'yum-ius'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Installs/Configures yum-ius'
+description 'Installs and configures the IUS Community Project Yum Repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.4.1'
 
@@ -11,3 +11,7 @@ depends 'yum-epel'
 
 source_url 'https://github.com/chef-cookbooks/yum-ius' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/yum-ius/issues' if respond_to?(:issues_url)
+
+%w(amazon centos fedora oracle redhat scientific).each do |os|
+  supports os
+end
