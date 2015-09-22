f3f399d992c837c68921bfe6dad65e9554079c18
Tim Smith <tsmith84@gmail.com>
2015-09-21 17:04:14 -0700

    Rubocop fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -30,38 +30,38 @@
   ius-dev-debuginfo
   ius-dev-source
 ).each do |repo|
-  if node['yum'][repo]['managed']
-    include_recipe 'yum-epel' unless run_context.loaded_recipe?('yum-epel')
+  next unless node['yum'][repo]['managed']
 
-    yum_repository repo do
-      description node['yum'][repo]['description']
-      baseurl node['yum'][repo]['baseurl']
-      mirrorlist node['yum'][repo]['mirrorlist']
-      gpgcheck node['yum'][repo]['gpgcheck']
-      gpgkey node['yum'][repo]['gpgkey']
-      enabled node['yum'][repo]['enabled']
-      cost node['yum'][repo]['cost']
-      exclude node['yum'][repo]['exclude']
-      enablegroups node['yum'][repo]['enablegroups']
-      failovermethod node['yum'][repo]['failovermethod']
-      http_caching node['yum'][repo]['http_caching']
-      include_config node['yum'][repo]['include_config']
-      includepkgs node['yum'][repo]['includepkgs']
-      keepalive node['yum'][repo]['keepalive']
-      max_retries node['yum'][repo]['max_retries']
-      metadata_expire node['yum'][repo]['metadata_expire']
-      mirror_expire node['yum'][repo]['mirror_expire']
-      priority node['yum'][repo]['priority']
-      proxy node['yum'][repo]['proxy']
-      proxy_username node['yum'][repo]['proxy_username']
-      proxy_password node['yum'][repo]['proxy_password']
-      repositoryid node['yum'][repo]['repositoryid']
-      sslcacert node['yum'][repo]['sslcacert']
-      sslclientcert node['yum'][repo]['sslclientcert']
-      sslclientkey node['yum'][repo]['sslclientkey']
-      sslverify node['yum'][repo]['sslverify']
-      timeout node['yum'][repo]['timeout']
-      action :create
-    end
+  include_recipe 'yum-epel' unless run_context.loaded_recipe?('yum-epel')
+
+  yum_repository repo do
+    description node['yum'][repo]['description']
+    baseurl node['yum'][repo]['baseurl']
+    mirrorlist node['yum'][repo]['mirrorlist']
+    gpgcheck node['yum'][repo]['gpgcheck']
+    gpgkey node['yum'][repo]['gpgkey']
+    enabled node['yum'][repo]['enabled']
+    cost node['yum'][repo]['cost']
+    exclude node['yum'][repo]['exclude']
+    enablegroups node['yum'][repo]['enablegroups']
+    failovermethod node['yum'][repo]['failovermethod']
+    http_caching node['yum'][repo]['http_caching']
+    include_config node['yum'][repo]['include_config']
+    includepkgs node['yum'][repo]['includepkgs']
+    keepalive node['yum'][repo]['keepalive']
+    max_retries node['yum'][repo]['max_retries']
+    metadata_expire node['yum'][repo]['metadata_expire']
+    mirror_expire node['yum'][repo]['mirror_expire']
+    priority node['yum'][repo]['priority']
+    proxy node['yum'][repo]['proxy']
+    proxy_username node['yum'][repo]['proxy_username']
+    proxy_password node['yum'][repo]['proxy_password']
+    repositoryid node['yum'][repo]['repositoryid']
+    sslcacert node['yum'][repo]['sslcacert']
+    sslclientcert node['yum'][repo]['sslclientcert']
+    sslclientkey node['yum'][repo]['sslclientkey']
+    sslverify node['yum'][repo]['sslverify']
+    timeout node['yum'][repo]['timeout']
+    action :create
   end
 end
