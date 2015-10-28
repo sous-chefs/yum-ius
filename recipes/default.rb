9694d278b895bb82f782b4b77204b3d502a90400
Sean OMeara <sean@chef.io>
2015-10-28 01:56:55 -0400

    Fixing Chef 13 nil property deprecation warnings

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Author:: Sean OMeara (<someara@chef.io>)
+# Author:: Sean OMeara (<sean@chef.io>)
 # Recipe:: yum-ius::default
 #
-# Copyright 2013-2015, Chef Software, Inc.
+# Copyright 2013, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,38 +30,37 @@
   ius-dev-debuginfo
   ius-dev-source
 ).each do |repo|
-  next unless node['yum'][repo]['managed']
-
+  next if node['yum'][repo]['managed']
   include_recipe 'yum-epel' unless run_context.loaded_recipe?('yum-epel')
 
   yum_repository repo do
-    description node['yum'][repo]['description']
-    baseurl node['yum'][repo]['baseurl']
-    mirrorlist node['yum'][repo]['mirrorlist']
-    gpgcheck node['yum'][repo]['gpgcheck']
-    gpgkey node['yum'][repo]['gpgkey']
-    enabled node['yum'][repo]['enabled']
-    cost node['yum'][repo]['cost']
-    exclude node['yum'][repo]['exclude']
-    enablegroups node['yum'][repo]['enablegroups']
-    failovermethod node['yum'][repo]['failovermethod']
-    http_caching node['yum'][repo]['http_caching']
-    include_config node['yum'][repo]['include_config']
-    includepkgs node['yum'][repo]['includepkgs']
-    keepalive node['yum'][repo]['keepalive']
-    max_retries node['yum'][repo]['max_retries']
-    metadata_expire node['yum'][repo]['metadata_expire']
-    mirror_expire node['yum'][repo]['mirror_expire']
-    priority node['yum'][repo]['priority']
-    proxy node['yum'][repo]['proxy']
-    proxy_username node['yum'][repo]['proxy_username']
-    proxy_password node['yum'][repo]['proxy_password']
-    repositoryid node['yum'][repo]['repositoryid']
-    sslcacert node['yum'][repo]['sslcacert']
-    sslclientcert node['yum'][repo]['sslclientcert']
-    sslclientkey node['yum'][repo]['sslclientkey']
-    sslverify node['yum'][repo]['sslverify']
-    timeout node['yum'][repo]['timeout']
+    description node['yum'][repo]['description'] if node['yum'][repo]['description']
+    baseurl node['yum'][repo]['baseurl'] if  node['yum'][repo]['baseurl']
+    mirrorlist node['yum'][repo]['mirrorlist'] if node['yum'][repo]['mirrorlist']
+    gpgcheck node['yum'][repo]['gpgcheck'] if  node['yum'][repo]['gpgcheck']
+    gpgkey node['yum'][repo]['gpgkey'] if  node['yum'][repo]['gpgkey']
+    enabled node['yum'][repo]['enabled'] if node['yum'][repo]['enabled']
+    cost node['yum'][repo]['cost'] if node['yum'][repo]['cost']
+    exclude node['yum'][repo]['exclude'] if node['yum'][repo]['exclude']
+    enablegroups node['yum'][repo]['enablegroups'] if  node['yum'][repo]['enablegroups']
+    failovermethod node['yum'][repo]['failovermethod'] if node['yum'][repo]['failovermethod']
+    http_caching node['yum'][repo]['http_caching'] if  node['yum'][repo]['http_caching']
+    include_config node['yum'][repo]['include_config'] if node['yum'][repo]['include_config']
+    includepkgs node['yum'][repo]['includepkgs'] if node['yum'][repo]['includepkgs']
+    keepalive node['yum'][repo]['keepalive'] if  node['yum'][repo]['keepalive']
+    max_retries node['yum'][repo]['max_retries'] if node['yum'][repo]['max_retries']
+    metadata_expire node['yum'][repo]['metadata_expire'] if node['yum'][repo]['metadata_expire']
+    mirror_expire node['yum'][repo]['mirror_expire'] if  node['yum'][repo]['mirror_expire']
+    priority node['yum'][repo]['priority'] if node['yum'][repo]['priority']
+    proxy node['yum'][repo]['proxy'] if node['yum'][repo]['proxy']
+    proxy_username node['yum'][repo]['proxy_username'] if  node['yum'][repo]['proxy_username']
+    proxy_password node['yum'][repo]['proxy_password'] if  node['yum'][repo]['proxy_password']
+    repositoryid node['yum'][repo]['repositoryid'] if node['yum'][repo]['repositoryid']
+    sslcacert node['yum'][repo]['sslcacert'] if node['yum'][repo]['sslcacert']
+    sslclientcert node['yum'][repo]['sslclientcert'] if node['yum'][repo]['sslclientcert']
+    sslclientkey node['yum'][repo]['sslclientkey'] if node['yum'][repo]['sslclientkey']
+    sslverify node['yum'][repo]['sslverify'] if node['yum'][repo]['sslverify']
+    timeout node['yum'][repo]['timeout'] if  node['yum'][repo]['timeout']
     action :create
   end
 end
