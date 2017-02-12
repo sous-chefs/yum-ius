4737444c6b8fd48050ae858e80e89e8a97688612
Tim Smith <tsmith@chef.io>
2017-02-12 06:49:55 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,14 +6,13 @@ description 'Installs and configures the IUS Community Project Yum Repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.0'
 
+%w(centos oracle redhat scientific).each do |os|
+  supports os
+end
+
 depends 'compat_resource', '>= 12.16.3'
 depends 'yum-epel'
 
 source_url 'https://github.com/chef-cookbooks/yum-ius'
 issues_url 'https://github.com/chef-cookbooks/yum-ius/issues'
-
-%w(centos oracle redhat scientific).each do |os|
-  supports os
-end
-
 chef_version '>= 12.1' if respond_to?(:chef_version)
