175f135c013ca412e199b93036bdf8ff1554fa24
Tim Smith <tsmith@chef.io>
2017-06-14 16:40:56 -0700

    Fix test-debug to use platform_version
    Also avoid spec deprecation warnings
    `
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/ius-testing-debuginfo.rb
+++ attributes/ius-testing-debuginfo.rb
@@ -5,10 +5,10 @@ default['yum']['ius-testing-debuginfo']['failovermethod'] = 'priority'
 default['yum']['ius-testing-debuginfo']['gpgkey'] = 'https://dl.iuscommunity.org/pub/ius/IUS-COMMUNITY-GPG-KEY'
 default['yum']['ius-testing-debuginfo']['gpgcheck'] = true
 default['yum']['ius-archive-debuginfo']['sslverify'] = false if node['platform_version'].to_i == 5
-default['yum']['ius-testing-debuginfo']['description'] = 'IUS Community Packages for Enterprise Linux $releasever - $basearch Testing Debug'
+default['yum']['ius-testing-debuginfo']['description'] = "IUS Community Packages for Enterprise Linux #{node['platform_version'].to_i} - $basearch Testing Debug"
 default['yum']['ius-testing-debuginfo']['mirrorlist'] = case node['platform']
                                                         when 'redhat'
-                                                          'https://mirrors.iuscommunity.org/mirrorlist/?repo=ius-el$releasever-testing-debuginfo&arch=$basearch&protocol=http'
+                                                          "https://mirrors.iuscommunity.org/mirrorlist/?repo=ius-el#{node['platform_version'].to_i}-testing-debuginfo&arch=$basearch&protocol=http"
                                                         else
-                                                          'https://mirrors.iuscommunity.org/mirrorlist/?repo=ius-centos$releasever-testing-debuginfo&arch=$basearch&protocol=http'
+                                                          "https://mirrors.iuscommunity.org/mirrorlist/?repo=ius-centos#{node['platform_version'].to_i}-testing-debuginfo&arch=$basearch&protocol=http"
                                                         end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,18 +4,18 @@ describe 'yum-ius::default' do
   context 'yum-ius::default uses default attributes' do
     cached(:chef_run) do
       ChefSpec::SoloRunner.new do |node|
-        node.normal['yum']['ius']['managed'] = true
-        node.normal['yum']['ius-debuginfo']['managed'] = true
-        node.normal['yum']['ius-source']['managed'] = true
-        node.normal['yum']['ius-archive']['managed'] = true
-        node.normal['yum']['ius-archive-debuginfo']['managed'] = true
-        node.normal['yum']['ius-archive-source']['managed'] = true
-        node.normal['yum']['ius-testing']['managed'] = true
-        node.normal['yum']['ius-testing-debuginfo']['managed'] = true
-        node.normal['yum']['ius-testing-source']['managed'] = true
-        node.normal['yum']['ius-dev']['managed'] = true
-        node.normal['yum']['ius-dev-debuginfo']['managed'] = true
-        node.normal['yum']['ius-dev-source']['managed'] = true
+        node.override['yum']['ius']['managed'] = true
+        node.override['yum']['ius-debuginfo']['managed'] = true
+        node.override['yum']['ius-source']['managed'] = true
+        node.override['yum']['ius-archive']['managed'] = true
+        node.override['yum']['ius-archive-debuginfo']['managed'] = true
+        node.override['yum']['ius-archive-source']['managed'] = true
+        node.override['yum']['ius-testing']['managed'] = true
+        node.override['yum']['ius-testing-debuginfo']['managed'] = true
+        node.override['yum']['ius-testing-source']['managed'] = true
+        node.override['yum']['ius-dev']['managed'] = true
+        node.override['yum']['ius-dev-debuginfo']['managed'] = true
+        node.override['yum']['ius-dev-source']['managed'] = true
       end.converge(described_recipe)
     end
 
@@ -41,19 +41,19 @@ describe 'yum-ius::default' do
 
   context 'yum-ius::default uses Redhat distro for RHEL6' do
     cached(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'redhat', version: '6.5') do |node|
-        node.normal['yum']['ius']['managed'] = true
-        node.normal['yum']['ius-debuginfo']['managed'] = true
-        node.normal['yum']['ius-source']['managed'] = true
-        node.normal['yum']['ius-archive']['managed'] = true
-        node.normal['yum']['ius-archive-debuginfo']['managed'] = true
-        node.normal['yum']['ius-archive-source']['managed'] = true
-        node.normal['yum']['ius-testing']['managed'] = true
-        node.normal['yum']['ius-testing-debuginfo']['managed'] = true
-        node.normal['yum']['ius-testing-source']['managed'] = true
-        node.normal['yum']['ius-dev']['managed'] = true
-        node.normal['yum']['ius-dev-debuginfo']['managed'] = true
-        node.normal['yum']['ius-dev-source']['managed'] = true
+      ChefSpec::SoloRunner.new(platform: 'redhat', version: '6.7') do |node|
+        node.override['yum']['ius']['managed'] = true
+        node.override['yum']['ius-debuginfo']['managed'] = true
+        node.override['yum']['ius-source']['managed'] = true
+        node.override['yum']['ius-archive']['managed'] = true
+        node.override['yum']['ius-archive-debuginfo']['managed'] = true
+        node.override['yum']['ius-archive-source']['managed'] = true
+        node.override['yum']['ius-testing']['managed'] = true
+        node.override['yum']['ius-testing-debuginfo']['managed'] = true
+        node.override['yum']['ius-testing-source']['managed'] = true
+        node.override['yum']['ius-dev']['managed'] = true
+        node.override['yum']['ius-dev-debuginfo']['managed'] = true
+        node.override['yum']['ius-dev-source']['managed'] = true
       end.converge(described_recipe)
     end
 
@@ -83,18 +83,18 @@ describe 'yum-ius::default' do
   context 'yum-ius::default uses CentOS distro for CentOS6' do
     cached(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'centos', version: '6.8') do |node|
-        node.normal['yum']['ius']['managed'] = true
-        node.normal['yum']['ius-debuginfo']['managed'] = true
-        node.normal['yum']['ius-source']['managed'] = true
-        node.normal['yum']['ius-archive']['managed'] = true
-        node.normal['yum']['ius-archive-debuginfo']['managed'] = true
-        node.normal['yum']['ius-archive-source']['managed'] = true
-        node.normal['yum']['ius-testing']['managed'] = true
-        node.normal['yum']['ius-testing-debuginfo']['managed'] = true
-        node.normal['yum']['ius-testing-source']['managed'] = true
-        node.normal['yum']['ius-dev']['managed'] = true
-        node.normal['yum']['ius-dev-debuginfo']['managed'] = true
-        node.normal['yum']['ius-dev-source']['managed'] = true
+        node.override['yum']['ius']['managed'] = true
+        node.override['yum']['ius-debuginfo']['managed'] = true
+        node.override['yum']['ius-source']['managed'] = true
+        node.override['yum']['ius-archive']['managed'] = true
+        node.override['yum']['ius-archive-debuginfo']['managed'] = true
+        node.override['yum']['ius-archive-source']['managed'] = true
+        node.override['yum']['ius-testing']['managed'] = true
+        node.override['yum']['ius-testing-debuginfo']['managed'] = true
+        node.override['yum']['ius-testing-source']['managed'] = true
+        node.override['yum']['ius-dev']['managed'] = true
+        node.override['yum']['ius-dev-debuginfo']['managed'] = true
+        node.override['yum']['ius-dev-source']['managed'] = true
       end.converge(described_recipe)
     end
 
