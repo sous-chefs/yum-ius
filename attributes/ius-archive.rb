18b2342675fcfe67e944dc8aaaca771663b96d30
Gavin Staniforth <gavin@usemarkup.com>
2019-09-07 10:25:19 +0100

    feat: support new IUS CDN
    Signed-off-by: Gavin Staniforth <gavin@usemarkup.com>

--- attributes/ius-archive.rb
+++ attributes/ius-archive.rb
@@ -2,13 +2,8 @@ default['yum']['ius-archive']['repositoryid'] = 'ius-archive'
 default['yum']['ius-archive']['enabled'] = false
 default['yum']['ius-archive']['managed'] = false
 default['yum']['ius-archive']['failovermethod'] = 'priority'
-default['yum']['ius-archive']['gpgkey'] = 'https://dl.iuscommunity.org/pub/ius/IUS-COMMUNITY-GPG-KEY'
+default['yum']['ius-archive']['gpgkey'] = "https://repo.ius.io/RPM-GPG-KEY-IUS-#{node['platform_version'].to_i}"
 default['yum']['ius-archive']['gpgcheck'] = true
 default['yum']['ius-archive-debuginfo']['sslverify'] = false if node['platform_version'].to_i == 5
 default['yum']['ius-archive']['description'] = "IUS Community Packages for Enterprise Linux #{node['platform_version'].to_i} - $basearch Archive"
-default['yum']['ius-archive']['mirrorlist'] = case node['platform']
-                                              when 'redhat'
-                                                "https://mirrors.iuscommunity.org/mirrorlist/?repo=ius-el#{node['platform_version'].to_i}-archive&arch=$basearch&protocol=http"
-                                              else
-                                                "https://mirrors.iuscommunity.org/mirrorlist/?repo=ius-centos#{node['platform_version'].to_i}-archive&arch=$basearch&protocol=http"
-                                              end
+default['yum']['ius-archive']['baseurl'] = "https://repo.ius.io/archive/#{node['platform_version'].to_i}/$basearch"
--- attributes/ius-testing.rb
+++ attributes/ius-testing.rb
@@ -2,13 +2,8 @@ default['yum']['ius-testing']['repositoryid'] = 'ius-testing'
 default['yum']['ius-testing']['enabled'] = false
 default['yum']['ius-testing']['managed'] = false
 default['yum']['ius-testing']['failovermethod'] = 'priority'
-default['yum']['ius-testing']['gpgkey'] = 'https://dl.iuscommunity.org/pub/ius/IUS-COMMUNITY-GPG-KEY'
+default['yum']['ius-testing']['gpgkey'] = "https://repo.ius.io/RPM-GPG-KEY-IUS-#{node['platform_version'].to_i}"
 default['yum']['ius-testing']['gpgcheck'] = true
 default['yum']['ius-archive-debuginfo']['sslverify'] = false if node['platform_version'].to_i == 5
 default['yum']['ius-testing']['description'] = "IUS Community Packages for Enterprise Linux #{node['platform_version'].to_i} - $basearch Testing"
-default['yum']['ius-testing']['mirrorlist'] = case node['platform']
-                                              when 'redhat'
-                                                "https://mirrors.iuscommunity.org/mirrorlist/?repo=ius-el#{node['platform_version'].to_i}-testing&arch=$basearch&protocol=http"
-                                              else
-                                                "https://mirrors.iuscommunity.org/mirrorlist/?repo=ius-centos#{node['platform_version'].to_i}-testing&arch=$basearch&protocol=http"
-                                              end
+default['yum']['ius-testing']['baseurl'] = "https://repo.ius.io/testing/#{node['platform_version'].to_i}/$basearch"
