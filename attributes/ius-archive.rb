0f592632b69c6b187515fb79794a5a017b2e042d
Gavin Staniforth <gavin@usemarkup.com>
2019-09-07 10:31:00 +0100

    wip: correct ius-archive key

--- attributes/ius-archive.rb
+++ attributes/ius-archive.rb
@@ -2,7 +2,7 @@ default['yum']['ius-archive']['repositoryid'] = 'ius-archive'
 default['yum']['ius-archive']['enabled'] = false
 default['yum']['ius-archive']['managed'] = false
 default['yum']['ius-archive']['failovermethod'] = 'priority'
-default['yum']['ius-archive']['gpgkey'] = 'https://dl.iuscommunity.org/pub/ius/IUS-COMMUNITY-GPG-KEY'
+default['yum']['ius-archive']['gpgkey'] = 'https://repo.ius.io/RPM-GPG-KEY-IUS-#{node['platform_version'].to_i}'
 default['yum']['ius-archive']['gpgcheck'] = true
 default['yum']['ius-archive-debuginfo']['sslverify'] = false if node['platform_version'].to_i == 5
 default['yum']['ius-archive']['description'] = "IUS Community Packages for Enterprise Linux #{node['platform_version'].to_i} - $basearch Archive"
