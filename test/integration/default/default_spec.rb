949cc023a3307f15d1f76cabe3d90846e1196fa8
Ryan <kennedy@osuosl.org>
2021-03-02 15:20:42 -0800

    overlooked cookstyle mistake

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -4,8 +4,8 @@ describe yum.repo('ius') do
   its('baseurl') { should cmp "https://repo.ius.io/#{os.release.to_i}/x86_64/" }
 end
 
-describe ini("/etc/yum.repos.d/ius.repo") do
-  its("ius.gpgkey") { should cmp "https://repo.ius.io/RPM-GPG-KEY-IUS-#{e_rel}" }
-  its("ius.gpgcheck") { should cmp 1 }
-  its("ius.failovermethod") { should cmp 'priority' }
+describe ini('/etc/yum.repos.d/ius.repo') do
+  its('ius.gpgkey') { should cmp "https://repo.ius.io/RPM-GPG-KEY-IUS-#{os.release.to_i}" }
+  its('ius.gpgcheck') { should cmp 1 }
+  its('ius.failovermethod') { should cmp 'priority' }
 end
